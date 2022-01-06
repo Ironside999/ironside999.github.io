6027c7007f7f4cd0fd9c2cf5321406f676193003
ironside999 <mohammadesfandiari890@gmail.com>
2022-01-06 23:17:07 +0330

    Add Error Handling By While Loop

--- src/com/first/Main.java
+++ src/com/first/Main.java
@@ -12,30 +12,48 @@ public class Main {
 
 
         Scanner scanner = new Scanner(System.in);
+        int principal;
+        float monthlyInterst;
+        int period;
 
-        System.out.print("Principal : ");
         //p
-        int principal = scanner.nextInt();
+       while (true) {
+           System.out.print("Principal : ");
+           principal = scanner.nextInt();
+           if (principal >=1_000 && principal <= 1_000_000) {
+               break;
+           }
+           System.out.println("please enter a number between 1,000 AND 1,000,000");
+       }
 
-        System.out.print("Annual Interest Rate : ");
-        //r
-        float annualInterestRate = scanner.nextFloat() / PERCENT / MONTH_IN_YEAR;
+       while (true) {
+           System.out.print("Annual Interest Rate : ");
+           //r
+           float annualInterestRate = scanner.nextFloat();
+           if (annualInterestRate >= 0 && annualInterestRate <= 30) {
+               monthlyInterst = annualInterestRate/ PERCENT / MONTH_IN_YEAR;
+                break;
+           }
+           System.out.println("please enter a number less or equal than 30 AND greater than or equal 0");
+       }
 
-        System.out.print("Period (Years) : ");
-        //n
-        int period = scanner.nextInt() * MONTH_IN_YEAR;
+       while (true) {
+           System.out.print("Period (Years) : ");
+           //n
+           byte year = scanner.nextByte();
+           if (year >= 1 && year <= 30) {
+               period = year * MONTH_IN_YEAR;
+               break;
+           }
+           System.out.println("please enter a number greater than 0 AND less than 30");
+       }
 
         double mortgage = principal
-                * (annualInterestRate * Math.pow(1 + annualInterestRate, period))
-                / (Math.pow(1 + annualInterestRate, period) - 1);
+                * (monthlyInterst * Math.pow(1 + monthlyInterst, period))
+                / (Math.pow(1 + monthlyInterst, period) - 1);
         String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortage is : " + formattedMortgage);
 
-//        double up = (Math.pow((1 + annualInterestRate), period)) * annualInterestRate;
-//        double down = (Math.pow((1 + annualInterestRate), period)) - 1;
-//        double result = principal * (up / down);
-//        String finalResult = NumberFormat.getCurrencyInstance().format(result);
-//        System.out.println("Mortage is : " + finalResult);
 
 
 
